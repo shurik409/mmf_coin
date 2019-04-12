025514c2bcc1aa91e2724243fcb711dcaeec6624
Aliaksandr Karaichentseu <aliaksandr.karaichentseu@soft-fx.lv>
2019-04-12 17:56:58 +0300

    api read

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const mongodbClient = require('./mongodb')
+
+
 const TelegramBot = require('node-telegram-bot-api');
 const qr = require('qr-image');
 const Jimp = require("jimp");
@@ -47,25 +50,28 @@ bot.onText(/\/start/, (msg, match) => {
     });
 });
 
+bot.onText(/\/balance/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    // console.log(msg);
+    // bot.
+    balance = await mongodbClient.getBalance(chatId);
+    console.log(3, balance);
+    bot.sendMessage(chatId, `Your balance: ${balance.balance}`);
+})
+
 bot.on('callback_query', (msg) => {
     const chatId = msg.message.chat.id;
     const data = msg.data;
     const [type, count] = data.split(' ')
     console.log(type, count);
     if(type === 'add'){
-        var qr_svg = qr.imageSync(`${type}-${count} `, { type: 'png' });
+        var qr_svg = qr.imageSync(`${type}-${count}`, { type: 'png' });
+        bot.sendPhoto(chatId, qr_svg);
+    } else if(type === 'minus'){
+        var qr_svg = qr.imageSync(`${type}-${count}`, { type: 'png' });
         bot.sendPhoto(chatId, qr_svg);
     }
     bot.sendMessage(chatId, `You ${msg.data} coin`);
-});
-
-bot.on('add5', (msg) => {
-    const chatId = msg.chat.id;
-    if(!msg.photo){
-        var qr_svg = qr.imageSync(`$Add 5`, { type: 'png' });
-        bot.sendPhoto(chatId, qr_svg);
-        bot.sendMessage(chatId, 'You add 5 coin');
-    }
 });
 
 bot.on('photo', (msg) => {
@@ -78,43 +84,35 @@ bot.on('photo', (msg) => {
     bot.getFile(msg.photo[msg.photo.length - 1].file_id).then(data => parseFile(chatId, data.file_path));
 })
 
-// bot.on('')
 
-var FileReader = require('filereader') , fileReader = new FileReader();
 
 async function parseFile(chatId, file_path){
     console.log(file_path);
-    fetch(`https://api.telegram.org/file/bot${token}/${file_path}`)
-    .then(res => res.arrayBuffer())
-    .then(async images => {
-        // console.log(1111);
-        // const img = await Jimp.read(images);
-        // const qrReader = new QRReader();
-        // console.log(2222);
+    fetch(`http://api.qrserver.com/v1/read-qr-code/?fileurl=https://api.telegram.org/file/bot${token}/${file_path}`)
+        .then(data => console.log(data))
+    // .then(res => res.arrayBuffer())
+    // .then(async images => {
+    //     Jimp.read(images, function(err, image) {
+    //         qrReader.callback = function(err, value) {
+    //             console.log(value);
+    //             if(value){
+    //                 console.log('Result', value.result);
+    //                 fromQrResult(chatId, value.result);
+    //                 bot.sendMessage(chatId, value.result)
+    //             } else {
+    //                 bot.sendMessage(chatId, 'Pls make rephoto')
+    //             }
+    //         };
+    //         qrReader.decode(image.bitmap);
+    //     });
+    // })
+}
 
-        // const value = await new Promise((resolve, reject) => {
-        //     console.log(3333);
-        //     qr.callback = (err, v) => err != null ? reject(err) : resolve(v);
-        //     console.log(44444);
-        //     qr.decode(img.bitmap);
-        // });
-
-        // console.log(55555);
-        // console.log(value);
-
-        Jimp.read(images, function(err, image) {
-            qrReader.callback = function(err, value) {
-                // console.log(value);
-                if(value){
-                    console.log('Result', value.result);
-                    bot.sendMessage(chatId, value.result)
-                } else {
-                    bot.sendMessage(chatId, 'Pls make rephoto')
-                }
-            };
-            // console.log(111);
-            // image.getBase64("image/jpeg", (data) => console.log(data));
-            qrReader.decode(image.bitmap);
-        });
-    })
+async function fromQrResult(chatId, result) {
+    const [type, count] = result.split('-');
+    if(type === 'add'){
+        mongodbClient.addToBalance(chatId, parseInt(count));
+    } else if(type === 'minus'){
+        mongodbClient.addToBalance(chatId, -parseInt(count));
+    }
 }
--- mongodb.js
+++ mongodb.js
@@ -1,34 +1,76 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://shuri409:Sashathisisi1@mmfcoins-i1asg.azure.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-    if(err) console.log(err);
-    const collection = client.db("mmfcoins").collection("users.balance");
-    // collection.insertOne({
-    //     id: 'test',
-    //     balance: 10
-    // })
-    collection.findOne({id: 'asd'})
-        .then(data => console.log(data));
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//     if(err) console.log(err);
+//     const collection = client.db("mmfcoins").collection("users.balance");
+//     // collection.insertOne({
+//     //     id: 'test',
+//     //     balance: 10
+//     // })
+//     collection.
+//     collection.findOne({id: 'asd'})
+//         .then(data => console.log(data));
 
-    // perform actions on the collection object
-    client.close();
-});
 
-function setNewUser(userId){
-    client.connect(err => {
-        if(err) console.log(err);
-        const userColection = client.db("mmfcoins").collection("users.balance");
+//     // perform actions on the collection object
+//     client.close();
+// });
 
-        userColection.findOne({id: userId})
-            .then(data=> {
-                if(!data){
-                    userColection.insertOne({
-                        id: userId,
-                        balance: 0,
-                        operations: ['Create']
-                    })
-                }
-            })
+async function setNewUser(userId, userColection){
+    // const userColection = client.db("mmfcoins").collection("users.balance");
+
+    let user = await userColection.insertOne({
+        id: `${userId}`,
+        balance: 0,
+        operations: ['Create']
     })
+    return user.ops[0];
 }
+
+const getBalance = async (userId) =>{
+    console.log(1, userId);
+
+    userData = await new Promise((res, rej) => {
+        client = new MongoClient(uri, { useNewUrlParser: true })
+        client.connect(async err => {
+            if(err) console.log(err);
+            const userColection = client.db("mmfcoins").collection("users.balance");
+            let data = await userColection.findOne({id: `${userId}`});
+            if(data){
+                console.log('user founded');
+                client.close();
+                res(data);
+            } else {
+                let newUser = await setNewUser(userId, userColection);
+                console.log('user created');
+                client.close();
+                res(newUser);
+            }
+        })
+    });
+
+    return userData;
+}
+
+const addToBalance = async (userId, count) => {
+    userData = await new Promise((res, rej) => {
+        client = new MongoClient(uri, { useNewUrlParser: true })
+        client.connect(async err => {
+            if(err) console.log(err);
+            const userColection = client.db("mmfcoins").collection("users.balance");
+            console.log(0);
+            let balance = await getBalance(userId)
+            balance.balance += count;
+            console.log(0, balance);
+            let data = await userColection.findOneAndReplace({id: `${userId}`}, balance);
+            console.log(1,data);
+            res(data.value)
+        })
+    });
+    console.log(2, userData);
+    return userData;
+}
+
+module.exports.getBalance = getBalance;
+module.exports.addToBalance = addToBalance;
