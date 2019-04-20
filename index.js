fdf6f80d3fe1a6de41348607fe5ae58e2513521d
shurik409 <shurik409@gmail.com>
2019-04-21 00:06:00 +0300

    Change base

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const mongodbClient = require('./mongodb')
 
-
 const TelegramBot = require('node-telegram-bot-api');
 const qr = require('qr-image');
-const Jimp = require("jimp");
-const fs = require('fs');
 const fetch = require('node-fetch');
-var QrCode = require('qrcode-reader');
-var qrReader = new QrCode();
-var Blob = require('blob');
-var Buffer = require('buffer/').Buffer;
-var toBuffer = require('blob-to-buffer')
 
 const token = '802358275:AAF50V5R3lPrTrmkhVjYoI1O3wlOGu5lUpQ';
 
@@ -26,9 +18,6 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 bot.onText(/\/start/, (msg, match) => {
     const chatId = msg.chat.id;
     
-    // bot.editMessageReplyMarkup({
-    //     keyboard: [[test]]
-    // })
     bot.sendMessage(chatId, 'Create balance', {
         reply_markup: {
             inline_keyboard : [[
@@ -52,41 +41,29 @@ bot.onText(/\/start/, (msg, match) => {
 
 bot.onText(/\/balance/, async (msg, match) => {
     const chatId = msg.chat.id;
-    // console.log(msg);
-    // bot.
-    balance = await mongodbClient.getBalance(chatId);
+    balance = await mongodbClient.getBalance(chatId, msg.from);
     console.log(3, balance);
     bot.sendMessage(chatId, `Your balance: ${balance.balance}`);
 })
 
-bot.on('callback_query', (msg) => {
+bot.on('callback_query', async (msg) => {
     const chatId = msg.message.chat.id;
     const data = msg.data;
     const [type, count] = data.split(' ')
     console.log(type, count);
-    if(type === 'add'){
-        var qr_svg = qr.imageSync(`${type}-${count}`, { type: 'png' });
-        bot.sendPhoto(chatId, qr_svg);
-    } else if(type === 'minus'){
-        var qr_svg = qr.imageSync(`${type}-${count}`, { type: 'png' });
-        bot.sendPhoto(chatId, qr_svg);
-    }
+    let qrCode = await mongodbClient.addQrCode(type === 'minus' ? `-${count}` : count, msg.from);
+    console.log(qrCode);
+    var qr_svg = qr.imageSync(`${qrCode.id}`, { type: 'png' });
+    bot.sendPhoto(chatId, qr_svg);
     bot.sendMessage(chatId, `You ${msg.data} coin`);
 });
 
 bot.on('photo', (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'You send a photo');
-    // bot.sendMessage(chatId, JSON.stringify(msg));
-    // bot.sendPhoto(chatId, msg.photo[0].file_id);
-    console.log(msg.photo);
-    console.log(msg.photo.length);
-    bot.getFile(msg.photo[msg.photo.length - 1].file_id).then(data => parseFile(chatId, data.file_path));
+    bot.getFile(msg.photo[msg.photo.length - 1].file_id).then(data => parseFile(chatId, data.file_path, msg));
 })
 
-
-
-async function parseFile(chatId, file_path){
+async function parseFile(chatId, file_path, msg){
     console.log(file_path);
     console.log(`http://api.qrserver.com/v1/read-qr-code/?fileurl=https://api.telegram.org/file/bot${token}/${file_path}`);
     fetch(`http://api.qrserver.com/v1/read-qr-code/?fileurl=https://api.telegram.org/file/bot${token}/${file_path}`)
@@ -95,33 +72,16 @@ async function parseFile(chatId, file_path){
             if(data[0].symbol[0].data){
                 value = data[0].symbol[0].data;
                 console.log('Result', value);
-                fromQrResult(chatId, value);
-                bot.sendMessage(chatId, value)
+                fromQrResult(chatId, value, msg);
             }
         });
-    // .then(res => res.arrayBuffer())
-    // .then(async images => {
-    //     Jimp.read(images, function(err, image) {
-    //         qrReader.callback = function(err, value) {
-    //             console.log(value);
-    //             if(value){
-    //                 console.log('Result', value.result);
-    //                 fromQrResult(chatId, value.result);
-    //                 bot.sendMessage(chatId, value.result)
-    //             } else {
-    //                 bot.sendMessage(chatId, 'Pls make rephoto')
-    //             }
-    //         };
-    //         qrReader.decode(image.bitmap);
-    //     });
-    // })
 }
 
-async function fromQrResult(chatId, result) {
-    const [type, count] = result.split('-');
-    if(type === 'add'){
-        mongodbClient.addToBalance(chatId, parseInt(count));
-    } else if(type === 'minus'){
-        mongodbClient.addToBalance(chatId, -parseInt(count));
+async function fromQrResult(chatId, result, msg) {
+    let queryResult = await mongodbClient.addToBalance(chatId, result, msg.from);
+    if(queryResult.added){
+        bot.sendMessage(chatId, `${queryResult.addedValue} успешно добавленно на ваш баланс`);
+    } else{
+        bot.sendMessage(chatId, queryResult.text);
     }
 }
--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://shuri409:Sashathisisi1@mmfcoins-i1asg.azure.mongodb.net/test?retryWrites=true";
+const crypto = require('crypto');
 // const client = new MongoClient(uri, { useNewUrlParser: true });
 // client.connect(err => {
 //     if(err) console.log(err);
@@ -17,11 +18,16 @@ const uri = "mongodb+srv://shuri409:Sashathisisi1@mmfcoins-i1asg.azure.mongodb.n
 //     client.close();
 // });
 
-async function setNewUser(userId, userColection){
+
+
+async function setNewUser(userId, userColection, from){
     // const userColection = client.db("mmfcoins").collection("users.balance");
 
     let user = await userColection.insertOne({
         id: `${userId}`,
+        firstName: from.first_name,
+        lastName: from.last_name,
+        userName: from.username,
         balance: 0,
         operations: [{
             type: 'Create',
@@ -31,14 +37,14 @@ async function setNewUser(userId, userColection){
     return user.ops[0];
 }
 
-const getBalance = async (userId) =>{
+const getBalance = async (userId, from) =>{
     console.log(1, userId);
 
     userData = await new Promise((res, rej) => {
         client = new MongoClient(uri, { useNewUrlParser: true })
         client.connect(async err => {
             if(err) console.log(err);
-            let balance = await getBalanceFromClient(client, userId);
+            let balance = await getBalanceFromClient(client, userId, from);
             client.close();
             res(balance);
         })
@@ -47,7 +53,7 @@ const getBalance = async (userId) =>{
     return userData;
 }
 
-const getBalanceFromClient = async (client, userId) => {
+const getBalanceFromClient = async (client, userId, from) => {
     const userColection = client.db("mmfcoins").collection("users.balance");
     let data = await userColection.findOne({id: `${userId}`});
     let balance;
@@ -56,7 +62,7 @@ const getBalanceFromClient = async (client, userId) => {
         console.log('user founded');
         balance = data;
     } else {
-        let newUser = await setNewUser(userId, userColection);
+        let newUser = await setNewUser(userId, userColection, from);
         console.log('user created');
         balance = newUser;
     }
@@ -64,26 +70,80 @@ const getBalanceFromClient = async (client, userId) => {
 }
 
 
-const addToBalance = async (userId, count) => {
+const addToBalance = async (userId, qrHash, from) => {
     userData = await new Promise((res, rej) => {
         client = new MongoClient(uri, { useNewUrlParser: true })
         client.connect(async err => {
             if(err) console.log(err);
             const userColection = client.db("mmfcoins").collection("users.balance");
-            let balance = await getBalanceFromClient(client, userId)
-            balance.balance += count;
-            balance.operations.push({
-                type:`Change balance on ${count}`,
-                time: new Date(Date.now()).toString()
-            })
-            let data = await userColection.findOneAndReplace({id: `${userId}`}, balance);
+            const qrCodesColection = client.db("mmfcoins").collection("qrcodes");
+            let qrCode = await qrCodesColection.findOne({id: qrHash});
+            let data;
+            if(!qrCode.activated){
+                let balance = await getBalanceFromClient(client, userId, from);
+                let count = parseInt(qrCode.value);
+                qrCode.activated = true;
+                
+                balance.balance += count;
+                balance.operations.push({
+                    type:`Change balance on ${count}`,
+                    time: new Date(Date.now()).toString()
+                })
+                await qrCodesColection.findOneAndReplace({id: qrHash}, qrCode);
+                let userBalace = await userColection.findOneAndReplace({id: `${userId}`}, balance);
+                data = Object.assign({
+                    added: true,
+                    text: 'Успешно',
+                    addedValue: count
+                }, userBalace.value);
+            } else {
+                data = {
+                    added: false,
+                    text: 'Этот QrCode уже использовался'
+                }
+            }
             client.close();
-            res(data.value)
+            res(data)
         })
     });
     console.log(2, userData);
     return userData;
 }
 
+const addQrCode = async(value, from) => {
+
+    userData = await new Promise((res, rej) => {
+        client = new MongoClient(uri, { useNewUrlParser: true })
+        client.connect(async err => {
+            if(err) console.log(err);
+            let balance = await setNewQrCodes(value, client, from);
+            client.close();
+            res(balance);
+        })
+    });
+
+    return userData;
+}
+
+async function setNewQrCodes(value, client, from){
+    const qrCodesColection = client.db("mmfcoins").collection("qrcodes");
+    let date = new Date(Date.now()).toString();
+    let random = Math.random().toString();
+    let hash = crypto.createHash('sha1').update(date + random).digest('hex');
+    let fromUser = `${from.first_name} ${from.last_name} `
+
+    let user = await qrCodesColection.insertOne({
+        id: hash,
+        time: date,
+        value: value,
+        activated: false,
+        creatorFirstName: from.first_name,
+        creatorLastName: from.last_name,
+        creatorUserName: from.username
+    })
+    return user.ops[0];
+}
+
 module.exports.getBalance = getBalance;
 module.exports.addToBalance = addToBalance;
+module.exports.addQrCode = addQrCode;
